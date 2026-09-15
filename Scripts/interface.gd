30fbedc0a5ddf4dcb18246c0683abf997a1a1bb0
Luan Resende Ferreira <luanjenner@gmail.com>
2026-09-15 13:46:25 -0300

    Menu

---
+++ Scripts/interface.gd
@@ -0,0 +1,14 @@
+extends Control
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/jogo.tscn")
+	pass
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/créditos.tscn")
+	pass 
+
+func _on_button_3_pressed() -> void:
+	get_tree().quit()
+	pass 
